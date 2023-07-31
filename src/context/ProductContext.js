0f1fb3ca0354ec4b3697c2c38c018f9d86ba46f9
MD Naimur Rahman <naimurtsc567@gmail.com>
2023-07-31 20:29:08 +0600

    readme file Added

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -5,8 +5,6 @@ export const BuilderContext = createContext();
 const ProductContext = ({ children }) => {
   const [builderProduct, setBuilderProduct] = useState([]);
 
-  console.log(builderProduct);
-
   const value = { builderProduct, setBuilderProduct };
   return (
     <BuilderContext.Provider value={value}>{children}</BuilderContext.Provider>
